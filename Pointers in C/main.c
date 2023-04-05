94d0e79fd30be6129e0f5c44c9fa59ffc8a4438c
Eslam <e.yasser997@gmail.com>
2023-04-05 21:01:33 +0200

    HackerRank Problems in c

---
+++ Pointers in C/main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void update(int *a,int *b) {
+     *a += *b; // a = a + b;
+     *b -= *a-*b; // b = a-2b
+
+    }
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
